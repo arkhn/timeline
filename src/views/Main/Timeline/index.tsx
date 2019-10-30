e981610ce5b596f4a8fdb0b33971c0ae1aca3649
alexis <alexisthual@gmail.com>
2019-10-31 00:12:12 +0100

    Mock division for documents and timeline

---
+++ src/views/Main/Timeline/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Timeline = () => {
+  return <div>timeline</div>;
+};
+
+export default Timeline;
--- src/views/Main/index.tsx
+++ src/views/Main/index.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 
 import Navbar from "src/components/Navbar";
+import DocumentBrowser from "./DocumentBrowser";
+import Timeline from "./Timeline";
+
+import "./style.less";
 
 const MainView = () => {
   return (
     <>
       <Navbar />
+      <div id="main-container">
+        <DocumentBrowser />
+        <Timeline />
+      </div>
     </>
   );
 };
